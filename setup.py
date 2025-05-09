6525d59f213976477c43ded5a1d5cd6de56803a1
Simon Wang <cufesimonwang@gmail.com>
2025-05-09 08:43:00 -0700

    chore: update project configuration and dependencies

--- setup.py
+++ setup.py
@@ -1,35 +1,40 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="graphrag",
     version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        "langchain>=0.1.0",
-        "openai>=1.0.0",
-        "pydantic>=2.0.0",
-        "pyyaml>=6.0.0",
-        "networkx>=3.0.0",
-        "pytest>=7.0.0",
-    ],
-    python_requires=">=3.8",
     author="Simon Wang",
     author_email="cufesimonwang@gmail.com",
-    description="A GraphRAG system for LEED documentation",
-    long_description=open("README.md").read(),
+    description="A knowledge graph-based RAG system for LEED documentation",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cufesimonwang/GraphRAG-for-LEED",
-    project_urls={
-        "Bug Tracker": "https://github.com/cufesimonwang/GraphRAG-for-LEED/issues",
-        "Documentation": "https://github.com/cufesimonwang/GraphRAG-for-LEED#readme",
-    },
+    packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "pytest>=7.0",
+            "black>=22.0",
+            "isort>=5.0",
+            "flake8>=4.0",
+            "mypy>=0.900",
+        ]
+    },
 ) 
